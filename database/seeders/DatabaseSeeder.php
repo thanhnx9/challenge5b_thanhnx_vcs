c30f130bcc4c4e637a5e45729a365efac6f29058
unknown <xuanthanh1228@gmail.com>
2021-02-22 11:38:39 +0700

    seed db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
-
+use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,45 +16,56 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->delete();
+        $this->createNewUsers();
     }
 
     protected function createNewUsers()
     {
-        $password = Hash::make('12345678'); // Default user password
+        $password = Hash::make('123456a@A'); // Default user password
 
         $d = [
 
             ['name' => 'Adminitrator',
                 'email' => 'admin@admin.com',
-                'username' => 'admin',
+                'userName' => 'admin',
                 'password' => $password,
-                'user_type' => '1',    //admin
+                'role_id' => '1',    //admin
                 'remember_token' => Str::random(10),
+                'created_at' => now(),
+            ],
+            ['name' => 'Teacher1',
+                'email' => 'teacher1@teacher.com',
+                'role_id' => '2', //teacher
+                'userName' => 'teacher1',
+                'password' => $password,
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+            ],
+            ['name' => 'Teacher2',
+                'email' => 'teacher2@teacher.com',
+                'role_id' => '2', //teacher
+                'userName' => 'teacher2',
+                'password' => $password,
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+            ],
+            ['name' => 'Student1',
+                'email' => 'student1@student.com',
+                'role_id' => '3',
+                'userName' => 'student1',
+                'password' => $password,
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
             ],
 
-            ['name' => 'Teacher Mike',
-                'email' => 'teacher@teacher.com',
-                'user_type' => '2', //teacher
-                'username' => 'teacher1',
+            ['name' => 'Student2',
+                'email' => 'student2@student.com',
+                'role_id' => '3',
+                'userName' => 'student2',
                 'password' => $password,
                 'remember_token' => Str::random(10),
-            ],
-
-            ['name' => 'Thanh Ngo',
-                'email' => 'thanhnx@student.com',
-                'user_type' => '3',
-                'username' => 'thanhnx',
-                'password' => $password,
-                'remember_token' => Str::random(10),
-            ],
-
-            ['name' => 'Minh Nam',
-                'email' => 'nam@student.com',
-                'user_type' => '3',
-                'username' => 'nam',
-                'password' => $password,
-                'code' => strtoupper(Str::random(10)),
-                'remember_token' => Str::random(10),
+                'created_at' => now(),
             ],
         ];
         DB::table('users')->insert($d);
